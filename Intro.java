2cf3966a5216c65de77aa93259f31631947e48c5
omar-rasin <omarasinyt@gmail.com>
2024-09-10 23:55:02 +0600

    Starting Java

--- Intro.java
+++ Intro.java
@@ -2,7 +2,7 @@ public class Intro {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello world");
+        System.out.println("Welcome to Java");
     }
     
 }
