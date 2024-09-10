45f47ac4c7abe651bb208252c8fae4ecc47b5be1
omar-rasin <omarasinyt@gmail.com>
2024-09-11 00:00:49 +0600

    Understanding the basic starting syntax of java

---
+++ basic_Syntax_1.java
@@ -0,0 +1,9 @@
+//Simple code to understand and practice the basic syntax of java
+
+public class basic_Syntax_1 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hi, I just started learning this language");
+    }
+}
