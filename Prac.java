269563c411084f69b279852fc9d3377a608c5704
omar-rasin <omarasinyt@gmail.com>
2024-09-14 21:13:55 +0600

    Basic Java practice

---
+++ Prac.java
@@ -0,0 +1,17 @@
+public class Prac {
+    
+    public static void main(String[] args) {
+        
+        String studentsName = "Jhonny";
+
+        double CGPA = 3.99;
+
+        int age = 12;
+
+        String uniName = "MITT";
+
+        System.out.println("A " + age + " year old student named " + studentsName + " studies at " + uniName + ", and has a CGPA of " + CGPA);
+
+    }
+
+}
