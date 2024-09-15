ca9a14a4e3691f93faf5a4409772b441cfc097aa
omar-rasin <omarasinyt@gmail.com>
2024-09-15 13:04:42 +0600

    Basic addition program in Java

---
+++ Add.java
@@ -0,0 +1,12 @@
+public class Add {
+    
+    public static void main(String[] args) {
+        
+        int num1 = 40;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+    }
+}
