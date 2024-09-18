89fd083e261221e01f57182bb599954d8d026c9b
omar-rasin <omarasinyt@gmail.com>
2024-09-18 22:25:41 +0600

    Basic program to find out area of square without user input

---
+++ Square_area_1.java
@@ -0,0 +1,10 @@
+public class Square_area_1 {
+    public static void main(String[] args) {
+        
+        double sideLength = 12.2;
+
+        double area = sideLength*sideLength;
+
+        System.out.println("The area of the sqaure is " + area);
+    }
+}
