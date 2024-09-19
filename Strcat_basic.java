4d5c5ac1633c1199ab378d61849b0a7f0799b391
omar-rasin <omarasinyt@gmail.com>
2024-09-19 21:29:39 +0600

    Basic string concatenation in java

---
+++ Strcat_basic.java
@@ -0,0 +1,12 @@
+public class Strcat_basic {
+    
+    public static void main(String[] args) {
+        
+        String first_name = "Omar";
+        String last_name = "Rasin";
+
+        String full_name = first_name + last_name;
+
+        System.out.println("My full name is: " + full_name);
+    }
+}
