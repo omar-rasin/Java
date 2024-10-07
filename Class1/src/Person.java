d4035fd432049d86fce554ea8241fd206afe804d
omar-rasin <omarasinyt@gmail.com>
2024-10-07 23:13:03 +0600

    Practice questions from lesson 1 of OOP

---
+++ Class1/src/Person.java
@@ -0,0 +1,13 @@
+/*Write a Java class Person with fields name, age, and a method displayInfo() that prints the person's information.
+Create an object of this class in Main and call the method. */
+
+public class Person {
+
+    String name;
+    int age;
+
+    public void displayinfo() {
+        System.out.println("The person's name is: " + name + "\nThe person's age is: " + age);
+    }
+    
+}
---
+++ Class1/src/PersonMain.java
@@ -0,0 +1,11 @@
+public class PersonMain {
+    public static void main(String[] args)  {
+        
+        Person p = new Person();
+
+        p.name = "Jack";
+        p.age = 10;
+
+        p.displayinfo();
+    }
+}
