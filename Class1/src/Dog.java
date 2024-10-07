c26882f3c3a4bc743ece175c1f86808d9fdad397
omar-rasin <omarasinyt@gmail.com>
2024-10-07 23:12:42 +0600

    Practice questions from lesson 1 of OOP

---
+++ Class1/src/Dog.java
@@ -0,0 +1,14 @@
+/*Create a class Dog with fields breed, age, and color. Write a method bark() that 
+prints "Woof!" when called. Instantiate multiple Dog objects and call the bark()
+method for each. */
+
+public class Dog {
+    
+    String breed;
+    int age;
+    String color;
+
+    public void bark() {
+        System.out.println("Woof!");
+    }
+}
---
+++ Class1/src/DogMain.java
@@ -0,0 +1,18 @@
+
+public class DogMain {
+    
+    public static void main(String[] args) {
+        
+        Dog d1 = new Dog();
+        Dog d2 = new Dog();
+        Dog d3 = new Dog();
+        Dog d4 = new Dog();
+
+        d1.bark();
+        d2.bark();
+        d3.bark();
+        d4.bark();
+
+
+    }
+}
