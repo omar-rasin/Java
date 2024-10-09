de532a3fe98a1e7c844a0ec0e128b52a466592b3
omar-rasin <omarasinyt@gmail.com>
2024-10-09 23:35:42 +0600

    Main file of the student program

---
+++ Class2/src/StudentMain.java
@@ -0,0 +1,9 @@
+public class StudentMain {
+    
+    public static void main(String[] args) {
+        
+        Student s = new Student("Jeffery" , 11 , 33.4);
+
+        s.displayinfo();
+    }
+}
