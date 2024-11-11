e86c33639609db7a6521ce88809db53ead047963
omar-rasin <omarasinyt@gmail.com>
2024-11-11 17:02:54 +0600

    Main file of Q2

---
+++ Class3/src/Main2.java
@@ -0,0 +1,11 @@
+public class Main2 {
+    
+    public static void main(String[] args) {
+        
+        Car c1 = new Car();
+        Bike b1 = new Bike();
+
+        c1.start();
+        b1.stop();
+    }
+}
