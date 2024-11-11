1df272548baaa7eebe388441aa6e0d9e04ce775f
omar-rasin <omarasinyt@gmail.com>
2024-11-11 17:03:59 +0600

    Main  file of Q4

---
+++ Class3/src/Main4.java
@@ -0,0 +1,11 @@
+public class Main4 {
+    
+    public static void main(String[] args) {
+        
+        Circle c1 = new Circle(3);
+        Rectangle r1 = new Rectangle(5,4);
+
+        c1.area();
+        r1.area();
+    }
+}
