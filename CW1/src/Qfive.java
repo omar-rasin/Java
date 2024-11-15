26e8a0a781a846bac4e4623d5293e3fa823116e7
omar-rasin <omarasinyt@gmail.com>
2024-11-16 00:51:03 +0600

    Q5 solution

---
+++ CW1/src/Qfive.java
@@ -0,0 +1,12 @@
+public class Qfive {
+    
+    public static void main(String[] args) {
+        
+        for(int i=0; i<=20; i++) {
+
+            if(i%2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
