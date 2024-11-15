79a7c9efb5007e18914979c97ceaed3d83bba4e5
omar-rasin <omarasinyt@gmail.com>
2024-11-16 00:50:20 +0600

    Q1 solution

---
+++ CW1/src/Qone.java
@@ -0,0 +1,12 @@
+public class Qone {
+   
+    public static void main(String[] args) {
+        
+        int num1 = 20;
+        int num2 = 30;
+
+        int sum = num1 + num2 ;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
